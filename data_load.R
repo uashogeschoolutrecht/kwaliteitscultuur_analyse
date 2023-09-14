abbc713170d6698db1588c5691dcbe785cbd9d9e
ALeemans <anne.leemans@hu.nl>
2023-09-14 10:08:19 +0200

    Script om data in te laden toegevoegd

---
+++ data_load.R
@@ -0,0 +1,10 @@
+# INLZEN DATA UIT SHAREPOINT
+
+library(sys)
+gebruiker = Sys.info()["user"]
+data_dir = paste("C:\\Users\\",gebruiker,"\\Stichting Hogeschool Utrecht\\FCA-DA-P - Analytics\\Analyse kwaliteitscultuur\\data",sep="")
+
+
+
+
+
